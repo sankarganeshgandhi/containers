b7696d378903ce280ff9adacc17933190454086d
Sankarganesh Gandhi <sankarganesh.gandhi@gmail.com>
2021-04-16 10:38:20 +0200

    initial version of batch script to start pgadmin along with psql

---
+++ docker-run-pgadmin.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo "Step 1- create a docker bridge network database-network "
+docker network create psql-network 
+
+echo "Step 2- create a docker conatiner: container-postgresdb "
+docker run  --detach --network psql-network ^
+  --name container-psql ^
+  --publish 5432:5432 ^
+  --env POSTGRES_USER=postgres ^
+  --env POSTGRES_PASSWORD=postgres ^
+    postgres:latest   
+
+ echo "Step 3- create a docker conatiner: container-pgadm "
+ docker run --detach ^
+   --network psql-network ^
+   --name container-pgadm ^
+   --publish 5050:80 ^
+   --env PGADMIN_DEFAULT_EMAIL=sankarganesh.gandhi@gmail.com ^
+   --env PGADMIN_DEFAULT_PASSWORD=admin ^
+     dpage/pgadmin4
+
+@echo on
