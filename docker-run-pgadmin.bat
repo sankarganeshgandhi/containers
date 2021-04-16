1c92fe79053eee2be4756048fb068d65b793c3d9
sgandhi <sankarganesh.gandhi@gmail.com>
2021-04-16 14:45:20 +0200

    Update docker-run-pgadmin.bat

--- docker-run-pgadmin.bat
+++ docker-run-pgadmin.bat
@@ -8,7 +8,7 @@ docker run  --detach --network psql-network ^
   --name container-psql ^
   --publish 5432:5432 ^
   --env POSTGRES_USER=postgres ^
-  --env POSTGRES_PASSWORD=postgres ^
+  --env POSTGRES_PASSWORD=****** ^
     postgres:latest   
 
  echo "Step 3- create a docker conatiner: container-pgadm "
@@ -17,7 +17,7 @@ docker run  --detach --network psql-network ^
    --name container-pgadm ^
    --publish 5050:80 ^
    --env PGADMIN_DEFAULT_EMAIL=sankarganesh.gandhi@gmail.com ^
-   --env PGADMIN_DEFAULT_PASSWORD=admin ^
+   --env PGADMIN_DEFAULT_PASSWORD=******* ^
      dpage/pgadmin4
 
 @echo on
